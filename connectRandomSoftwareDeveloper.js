29d8058ae174b5d0911758e528e7f6c7a1287c83
achiyahb <achiyahb@gmail.com>
2020-11-03 12:34:55 +0200

    change the connect number to 80

--- connectRandomSoftwareDeveloper.js
+++ connectRandomSoftwareDeveloper.js
@@ -1,16 +1,90 @@
+let cardIndexThatChecked =null
+let relevantMembers = []
+let relevantMembersCounter = 0
+let numberClicksGoal = 80
+let allClicksCounter = null
+let windowCard =  document.querySelector('.artdeco-modal__content.discover-cohort-recommendations-modal__content')
+let pointY = 100
 
-function ticker(secToPlay){
-    console.log('secToPlay',secToPlay/1000)
-    setTimeout(()=> {
+connectRelevantMembers()
 
-        SetNewIntervalRandomly()
-    },secToPlay)
+async function connectRelevantMembers() {
+    let shuffledArray = await setShuffledArray()
+    scrollDown()
+    if (!shuffledArray.length){
+        setTimeout(()=>connectRelevantMembers(),500)
+        return
+    }
+    SetNewIntervalRandomly(shuffledArray)
 }
 
-ticker(3000)
+function scrollDown(){
+    pointY += 100
+    windowCard.scroll(0,pointY)
+}
 
-function SetNewIntervalRandomly(){
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+function setShuffledArray(){
+    relevantMembers = []
+    let membersCards = windowCard.querySelectorAll('.discover-entity-type-card');
+    let REConnect = /Connect/
+
+    membersCards.forEach((card,key)=>{
+        if(REConnect.test(card.innerHTML) && key%3 !== 0 && key > cardIndexThatChecked){
+            relevantMembers.push(card)
+        }
+    })
+    cardIndexThatChecked = membersCards.length
+    shuffle(relevantMembers)
+    return relevantMembers
+}
+
+
+
+function ticker(someSecBeforeClick,shuffledArray){
+    console.log('secToPlay',someSecBeforeClick/1000)
+    setTimeout(()=> {
+        let connectButton = shuffledArray[relevantMembersCounter].querySelector('.full-width');
+        connectButton.click();
+        // console.log(connectButton)
+        relevantMembersCounter++
+        allClicksCounter++
+        if (allClicksCounter >+ numberClicksGoal){
+            console.log('finish')
+
+        } else if (relevantMembersCounter >= relevantMembers.length) {
+            relevantMembersCounter = 0
+            scrollDown()
+            setTimeout(()=>connectRelevantMembers(),1000)
+        } else {
+            SetNewIntervalRandomly(shuffledArray)
+        }
+    },someSecBeforeClick)
+}
+
+
+
+function SetNewIntervalRandomly(shuffledArray){
     let randomNumberBetween1to10 = Math.random() * 10
-    let randomSecToPlay = 1000 * randomNumberBetween1to10
-    ticker(randomSecToPlay)
+    let randomSecToPlay = 300 * randomNumberBetween1to10 + 1500
+    ticker(randomSecToPlay,shuffledArray)
 }
--- connectToSDeveloper.js
+++ connectToSDeveloper.js
@@ -1,90 +1,17 @@
-let cardIndexThatChecked =null
-let relevantMembers = []
-let relevantMembersCounter = 0
-let numberClicksGoal = 80
-let allClicksCounter = null
-let windowCard =  document.querySelector('.artdeco-modal__content.discover-cohort-recommendations-modal__content')
-let pointY = 100
-
-connectRelevantMembers()
-
-async function connectRelevantMembers() {
-    let shuffledArray = await setShuffledArray()
-    scrollDown()
-    if (!shuffledArray.length){
-        setTimeout(()=>connectRelevantMembers(),500)
-        return
+let scrollCounter = 0
+let connectNumber = 80
+let scrollDown = setInterval(()=>{
+    document.querySelector('.artdeco-modal_content.discover-cohort-recommendations-modal_content').scroll(0,50000)
+    scrollCounter++
+    if(scrollCounter >20){
+        clearInterval(scrollDown)
+        hitTheConnectButton()
     }
-    SetNewIntervalRandomly(shuffledArray)
-}
-
-function scrollDown(){
-    pointY += 100
-    windowCard.scroll(0,pointY)
-}
-
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
-function setShuffledArray(){
-    relevantMembers = []
-    let membersCards = windowCard.querySelectorAll('.discover-entity-type-card');
-    let REConnect = /Connect/
-
-    membersCards.forEach((card,key)=>{
-        if(REConnect.test(card.innerHTML) && key%3 !== 0 && key > cardIndexThatChecked){
-            relevantMembers.push(card)
-        }
-    })
-    cardIndexThatChecked = membersCards.length
-    shuffle(relevantMembers)
-    return relevantMembers
-}
-
-
-
-function ticker(someSecBeforeClick,shuffledArray){
-    console.log('secToPlay',someSecBeforeClick/1000)
-    setTimeout(()=> {
-        let connectButton = shuffledArray[relevantMembersCounter].querySelector('.full-width');
+},1000)
+function hitTheConnectButton() {
+    let membersCards = document.querySelectorAll('.discover-entity-type-card');
+    for (let i = 0; i < connectNumber; i++) {
+        let connectButton = membersCards[i].querySelector('.full-width');
         connectButton.click();
-        // console.log(connectButton)
-        relevantMembersCounter++
-        allClicksCounter++
-        if (allClicksCounter >+ numberClicksGoal){
-            console.log('finish')
-
-        } else if (relevantMembersCounter >= relevantMembers.length) {
-            relevantMembersCounter = 0
-            scrollDown()
-            setTimeout(()=>connectRelevantMembers(),1000)
-        } else {
-            SetNewIntervalRandomly(shuffledArray)
-        }
-    },someSecBeforeClick)
-}
-
-
-
-function SetNewIntervalRandomly(shuffledArray){
-    let randomNumberBetween1to10 = Math.random() * 10
-    let randomSecToPlay = 300 * randomNumberBetween1to10 + 1500
-    ticker(randomSecToPlay,shuffledArray)
+    }
 }
