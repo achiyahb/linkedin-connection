1d8763e113bf13be922ba5a37171e370fa670aa2
achiyahb <achiyahb@gmail.com>
2020-11-04 09:33:43 +0200

    after create my first chrome extension

--- chrome_extention/connectToDevsScrollCorrectly.js
+++ chrome_extention/connectToDevsScrollCorrectly.js
@@ -2,19 +2,30 @@ let startFromIndex = 0
 let endFromIndex = 7
 let numberClicksGoal = 80
 let allClicksCounter = null
-let windowCard = document.querySelector('.artdeco-modal__content.discover-cohort-recommendations-modal__content')
+let windowCard
 let pixelAddToGoalCoord = 587.5
 let index = 0
 let positionY = 0
+let agreementToTerm = 'trail'
+let trailMode = true
+let REReal = /^real$/
 
-
+function startToConnect(){
+    windowCard = document.querySelector('.artdeco-modal__content.discover-cohort-recommendations-modal__content')
+    numberClicksGoal = prompt('how many people you wish to connect? we recommend less then 80','80')
+    agreementToTerm = prompt('Are you interested in running the bot in trial or real mode? For real mode, write real.\n' +
+        'By sending this message, you confirm that you are aware that the use of this bot is at your own responsibility.','trail')
+    trailMode = !REReal.test(agreementToTerm.toLowerCase())
+    trailMode ? console.log('trail mode start') : console.log('real mode start')
+    connectToEightMembers()
+}
 
 function connectToEightMembers(){
     console.log('startFromIndex:',startFromIndex)
     console.log('endFromIndex:',endFromIndex)
     let membersCards = windowCard.querySelectorAll('.discover-entity-type-card');
     let indexNumberArray = []
-    for (let i = startFromIndex; i <= endFromIndex; i++){
+    for (let i = endFromIndex; i >= startFromIndex; i--){
         let randomBoolean = Math.random() >= 0.5;
         if (randomBoolean){
             indexNumberArray.push(i)
@@ -39,8 +50,7 @@ function ticker(someSecBeforeClick,indexNumberArray,membersCards){
     setTimeout(()=> {
         let i = indexNumberArray[index]
         let connectButton = membersCards[i].querySelector('.full-width');
-        // connectButton.click();
-        console.log(connectButton)
+        trailMode ? console.log(connectButton) : connectButton.click();
         allClicksCounter++
         index++
         if (allClicksCounter >+ numberClicksGoal){
@@ -63,5 +73,8 @@ function scrollDown(){
     },1000)
 }
 
+chrome.runtime.onMessage.addListener(function (request) {
+    startToConnect()
+})
 
-
+console.log('connector is activated')
---
+++ chrome_extention/popup.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', onclick, false)
+    function onclick () {
+      chrome.tabs.query({currentWindow: true, active: true},
+          function (tabs) {
+          console.log('log')
+        chrome.tabs.sendMessage(tabs[0].id, 'hi')
+      })
+    }
+}, false)
+
+
